30e71e6a137d9466788e2336efee1b641f941f51
daniele0118 <aheipo@yahoo.com.br>
2024-05-29 11:36:09 -0300

    Organizadas as rotas de aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -11,20 +11,20 @@ function App() {
 
   return (
     <>
-          <BrowserRouter>
-            <Navbar />
-              <div className="min-h-[80vh]">
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/home" element={<Home />} />
-                  <Route path="/categorias" element={<ListarCategorias />} />
-                  <Route path="/cadastrarcategoria" element={<FormCategoria />} />
-                  <Route path="/editarcategoria/:id" element={<FormCategoria />} />
-                  <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
-                </Routes>
-              </div>
-              <Footer />
-          </BrowserRouter>
+        <BrowserRouter>
+          <Navbar />
+            <div className="min-h-[80vh]">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/categorias" element={<ListarCategorias />} />
+                <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+                <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+                <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
+              </Routes>
+            </div>
+            <Footer />
+        </BrowserRouter>
     </>
   )
 }
